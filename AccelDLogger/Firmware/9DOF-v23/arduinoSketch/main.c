a5e9f5eb99c63bba9eaf6101b269352de2492bac
Michael Audi <maudi@sidevices.com>
2014-10-30 23:17:40 -0400

    ADXL345 3Axis 200Hz Data Logger
    57600 BAUD

---
+++ AccelDLogger/Firmware/9DOF-v23/arduinoSketch/main.c
@@ -0,0 +1,358 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include "types.h"
+#include "defs.h"
+#include "i2c.h"
+
+#define STATUS_LED 5 //stat LED is on PB5
+
+#define sbi(var, mask)   ((var) |= (uint8_t)(1 << mask))
+#define cbi(var, mask)   ((var) &= (uint8_t)~(1 << mask))
+
+#define ITG3200_R 0xD1	// ADD pin is pulled low
+#define ITG3200_W 0xD0 
+
+///============Initialize Prototypes=====//////////////////
+void init(void);
+unsigned int UART_Init(unsigned int ubrr);
+uint8_t uart_getchar(void);
+static int uart_putchar(char c, FILE *stream);
+static FILE mystdout = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
+void i2cInit(void);
+
+///============Function Prototypes=========/////////////////
+void accelerometer_init(void);
+uint16_t x_accel(void);
+uint16_t y_accel(void);
+uint16_t z_accel(void);
+uint8_t newData(void);
+void print_adxl345(void);
+
+/////===========MAIN=====================/////////////////////
+int main(void)
+{
+	init();
+	UART_Init(16);
+	while(1)
+	{	
+		while(!newData());
+                print_adxl345();
+	}
+}
+
+void accelerometer_init(void)
+{
+
+	//initialize
+	i2cSendStart();
+	i2cWaitForComplete();
+	i2cSendByte(0xA6);    //write to ADXL
+	i2cWaitForComplete();
+	i2cSendByte(0x2D);    //power register
+	i2cWaitForComplete();
+	i2cSendByte(0x08);    //measurement mode
+	i2cWaitForComplete();
+	i2cSendStop();
+	
+	i2cSendStart();
+	i2cWaitForComplete();
+	i2cSendByte(0xA6);    //write to ADXL
+	i2cWaitForComplete();
+	i2cSendByte(0x31);    //data format
+	i2cWaitForComplete();
+	i2cSendByte(0x0B);    //full resolution
+	i2cWaitForComplete();
+	i2cSendStop();
+
+	i2cSendStart();
+	i2cWaitForComplete();
+	i2cSendByte(0xA6);    //write to ADXL
+	i2cWaitForComplete();
+	i2cSendByte(0x2C);    //Set Data Rate
+	i2cWaitForComplete();
+	i2cSendByte(0x0B);    //200 Hz
+	i2cWaitForComplete();
+	i2cSendStop();
+
+	i2cSendStart();
+	i2cWaitForComplete();
+	i2cSendByte(0xA6);    //write to ADXL
+	i2cWaitForComplete();
+	i2cSendByte(0x2E);    //Enable Data Ready Interrupt
+	i2cWaitForComplete();
+	i2cSendByte(0x80);    
+	i2cWaitForComplete();
+	i2cSendStop();
+}
+
+void print_adxl345(void)
+{	
+	printf("%6d,", x_accel());
+	printf("%6d,", y_accel());
+	printf("%6d\n\r", z_accel());
+}
+
+uint8_t newData(void){
+        uint8_t dummy, xl;
+        
+  	i2cSendStart();
+	i2cWaitForComplete();
+	i2cSendByte(0xA6);    //write to ADXL
+	i2cWaitForComplete();
+	i2cSendByte(0x30);    //X0 data register
+	i2cWaitForComplete();
+	
+	i2cSendStop();		 //repeat start
+	i2cSendStart();
+
+	i2cWaitForComplete();
+	i2cSendByte(0xA7);    //read from ADXL
+	i2cWaitForComplete();
+	i2cReceiveByte(TRUE);
+	i2cWaitForComplete();
+	xl = i2cGetReceivedByte();	//x low byte
+	i2cWaitForComplete();
+	i2cReceiveByte(FALSE);
+	i2cWaitForComplete();
+	dummy = i2cGetReceivedByte();	//must do a multiple byte read?
+	i2cWaitForComplete();
+	i2cSendStop();	
+
+        if(xl & 0x80)
+          return 1;
+        else
+          return 0;
+}
+
+uint16_t x_accel(void)
+{
+	//0xA6 for a write
+	//0xA7 for a read
+
+	uint8_t dummy, xh, xl;
+	uint16_t xo;
+
+	//0x32 data registers
+	i2cSendStart();
+	i2cWaitForComplete();
+	i2cSendByte(0xA6);    //write to ADXL
+	i2cWaitForComplete();
+	i2cSendByte(0x32);    //X0 data register
+	i2cWaitForComplete();
+	
+	i2cSendStop();		 //repeat start
+	i2cSendStart();
+
+	i2cWaitForComplete();
+	i2cSendByte(0xA7);    //read from ADXL
+	i2cWaitForComplete();
+	i2cReceiveByte(TRUE);
+	i2cWaitForComplete();
+	xl = i2cGetReceivedByte();	//x low byte
+	i2cWaitForComplete();
+	i2cReceiveByte(FALSE);
+	i2cWaitForComplete();
+	dummy = i2cGetReceivedByte();	//must do a multiple byte read?
+	i2cWaitForComplete();
+	i2cSendStop();	
+	
+	//0x33 data registers
+	i2cSendStart();
+	i2cWaitForComplete();
+	i2cSendByte(0xA6);    //write to ADXL
+	i2cWaitForComplete();
+	i2cSendByte(0x33);    //X1 data register
+	i2cWaitForComplete();
+	
+	i2cSendStop();		 //repeat start
+	i2cSendStart();
+
+	i2cWaitForComplete();
+	i2cSendByte(0xA7);    //read from ADXL
+	i2cWaitForComplete();
+	i2cReceiveByte(TRUE);
+	i2cWaitForComplete();
+	xh = i2cGetReceivedByte();	//x high byte
+	i2cWaitForComplete();
+	i2cReceiveByte(FALSE);
+	i2cWaitForComplete();
+	dummy = i2cGetReceivedByte();	//must do a multiple byte read?
+	i2cWaitForComplete();
+	i2cSendStop();
+	xo = xl|(xh << 8);
+	return xo;
+}
+
+uint16_t y_accel(void)
+{		
+	//0xA6 for a write
+	//0xA7 for a read
+	
+	uint8_t dummy, yh, yl;
+	uint16_t yo;
+	
+	//0x34 data registers
+	i2cSendStart();
+	i2cWaitForComplete();
+	i2cSendByte(0xA6);    //write to ADXL
+	i2cWaitForComplete();
+	i2cSendByte(0x34);    //Y0 data register
+	i2cWaitForComplete();
+	
+	i2cSendStop();		 //repeat start
+	i2cSendStart();
+
+	i2cWaitForComplete();
+	i2cSendByte(0xA7);    //read from ADXL
+	i2cWaitForComplete();
+	i2cReceiveByte(TRUE);
+	i2cWaitForComplete();
+	yl = i2cGetReceivedByte();	//x low byte
+	i2cWaitForComplete();
+	i2cReceiveByte(FALSE);
+	i2cWaitForComplete();
+	dummy = i2cGetReceivedByte();	//must do a multiple byte read?
+	i2cWaitForComplete();
+	i2cSendStop();	
+	
+	//0x35 data registers
+	i2cSendStart();
+	i2cWaitForComplete();
+	i2cSendByte(0xA6);    //write to ADXL
+	i2cWaitForComplete();
+	i2cSendByte(0x35);    //Y1 data register
+	i2cWaitForComplete();
+	
+	i2cSendStop();		 //repeat start
+	i2cSendStart();
+
+	i2cWaitForComplete();
+	i2cSendByte(0xA7);    //read from ADXL
+	i2cWaitForComplete();
+	i2cReceiveByte(TRUE);
+	i2cWaitForComplete();
+	yh = i2cGetReceivedByte();	//y high byte
+	i2cWaitForComplete();
+	i2cReceiveByte(FALSE);
+	i2cWaitForComplete();
+	dummy = i2cGetReceivedByte();	//must do a multiple byte read?
+	i2cWaitForComplete();
+	i2cSendStop();
+	yo = yl|(yh << 8);
+	return yo;
+}
+
+uint16_t z_accel(void)
+{	
+	//0xA6 for a write
+	//0xA7 for a read
+	
+	uint8_t dummy, zh, zl;
+	uint16_t zo;
+	
+	//0x36 data registers
+	i2cSendStart();
+	i2cWaitForComplete();
+	i2cSendByte(0xA6);    //write to ADXL
+	i2cWaitForComplete();
+	i2cSendByte(0x36);    //Z0 data register
+	i2cWaitForComplete();
+	
+	i2cSendStop();		 //repeat start
+	i2cSendStart();
+
+	i2cWaitForComplete();
+	i2cSendByte(0xA7);    //read from ADXL
+	i2cWaitForComplete();
+	i2cReceiveByte(TRUE);
+	i2cWaitForComplete();
+	zl = i2cGetReceivedByte();	//z low byte
+	i2cWaitForComplete();
+	i2cReceiveByte(FALSE);
+	i2cWaitForComplete();
+	dummy = i2cGetReceivedByte();	//must do a multiple byte read?
+	i2cWaitForComplete();
+	i2cSendStop();	
+	
+	//0x37 data registers
+	i2cSendStart();
+	i2cWaitForComplete();
+	i2cSendByte(0xA6);    //write to ADXL
+	i2cWaitForComplete();
+	i2cSendByte(0x37);    //Z1 data register
+	i2cWaitForComplete();
+	
+	i2cSendStop();		 //repeat start
+	i2cSendStart();
+
+	i2cWaitForComplete();
+	i2cSendByte(0xA7);    //read from ADXL
+	i2cWaitForComplete();
+	i2cReceiveByte(TRUE);
+	i2cWaitForComplete();
+	zh = i2cGetReceivedByte();	//z high byte
+	i2cWaitForComplete();
+	i2cReceiveByte(FALSE);
+	i2cWaitForComplete();
+	dummy = i2cGetReceivedByte();	//must do a multiple byte read?
+	i2cWaitForComplete();
+	i2cSendStop();
+	zo = zl|(zh << 8);	
+	return zo;
+}
+
+/*********************
+ ****Initialize****
+ *********************/
+ 
+void init (void)
+{
+    //1 = output, 0 = input
+	DDRB = 0b01100000; //PORTB4, B5 output for stat LED
+    DDRC = 0b00010000; //PORTC4 (SDA), PORTC5 (SCL), PORTC all others are inputs
+    DDRD = 0b00000010; //PORTD (TX output on PD1)
+	PORTC = 0b00110000; //pullups on the I2C bus
+	
+	cbi(PORTB, 5);
+	
+	i2cInit();
+	accelerometer_init();
+}
+
+unsigned int UART_Init(unsigned int ubrr)
+{
+	int ubrr_new;
+	// set baud rate
+	ubrr_new = ubrr; 
+	UBRR0H = ubrr_new>>8;
+	UBRR0L = ubrr_new;
+	
+	// Enable receiver and transmitter 
+	UCSR0A = (1<<U2X0); //double the speed
+	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+	
+	// Set frame format: 8 bit, no parity, 1 stop bit,   
+	UCSR0C = (1<<UCSZ00)|(1<<UCSZ01);
+	
+	stdout = &mystdout; //Required for printf init
+	return(ubrr);
+}
+
+uint8_t uart_getchar(void)
+{
+    while( !(UCSR0A & (1<<RXC0)) );
+    return(UDR0);
+}
+
+static int uart_putchar(char c, FILE *stream)
+{
+    if (c == '\n') uart_putchar('\r', stream);
+  
+    loop_until_bit_is_set(UCSR0A, UDRE0);
+    UDR0 = c;
+    
+    return 0;
+}
+
